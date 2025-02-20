96e59ce5d7d8bbbf4a906a0457e9652e72d887e5
Anton Flavia <antonflavia20@stud.ase.ro>
2025-02-20 22:13:20 +0200

    feedback form popup

--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
+"use client";
+import Button from "./components/Button";
+import FeedbackFormPopUp from "./components/FeedBackFormPopUp";
 import FeedbackItem from "./components/FeedbackItem";
+import { useState } from "react";
 
 export default function Home() {
+  const [showFeedbackPopUp, setShowFeedbackPopUp] = useState(false);
+  function openFeedbackPopUp() {
+    setShowFeedbackPopUp(true);
+  }
   return (
-    <main className="bg-white md:max-w-2xl mx-auto md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
+    <main className="bg-white md:max-w-2xl md:mx-auto md:shadow-lg md:rounded-lg md:mt-8 overflow-hidden">
       <div className="bg-gradient-to-r from-pink-500 to-orange-400 p-8">
         <h1 className="font-bold text-xl">My App</h1>
         <p className="text-opacity-90 text-slate-700">
@@ -12,9 +20,9 @@ export default function Home() {
       <div className="bg-gray-100 px-8 py-4 flex border-b">
         <div className="grow"></div>
         <div>
-          <button className="bg-pink-500 py-1 px-4 rounded-md text-white text-opacity-90">
+          <Button primary onClick={openFeedbackPopUp}>
             Make a suggestion
-          </button>
+          </Button>
         </div>
       </div>
       <div className="px-8">
@@ -24,6 +32,9 @@ export default function Home() {
         <FeedbackItem></FeedbackItem>
         <FeedbackItem></FeedbackItem>
       </div>
+      {showFeedbackPopUp && (
+        <FeedbackFormPopUp setShow={setShowFeedbackPopUp} />
+      )}
     </main>
   );
 }
